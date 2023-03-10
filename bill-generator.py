14a34ff18ed13f5900d4ee1cad6cd44cddf48c42
donfortune <donfortunet.df@gmail.com>
2023-03-10 14:40:18 +0100

    first commit

---
+++ bill-generator.py
@@ -0,0 +1,11 @@
+
+num_of_eaters = input('Please how many people are having dinner? ')
+bill = input ('Your total bill is $150')
+tip_icl_total_bill  = 150 / 5 * 1.12
+rounded_num = round(tip_icl_total_bill, 2)
+print("your bill per person is :", rounded_num)
+
+
+
+
+
