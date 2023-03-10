b2eaae8d48868220a7ff7abbafc593779d643cc3
donfortune <donfortunet.df@gmail.com>
2023-03-10 13:48:54 +0100

    first commit

---
+++ band-name-generator.py
@@ -0,0 +1,4 @@
+print('HURRAY! GET TO KNOW YOUR BAND NAME')
+city = input('what city did you grow up in? \n' )
+pet = input('whats the name of your favorite pet? \n' )
+print(f"your band name is {city} {pet}")
